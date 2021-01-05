5d5c1cfabf95aecea614853329a95bd335d1a4f1
ahmedmonm <75921852+ahmedmonm@users.noreply.github.com>
2021-01-05 18:06:38 +0200

    text

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 import click
 import smtplib
-import re
-import getpass
-
-
-# This function used to validate the format of email
-def validateEmail(email):
-    return re.match('[\\w-]{1,20}@\\w{2,20}\\.\\w{2,3}$', email)
+from validate_email import validate_email
 
 
 @click.command()
@@ -18,34 +12,23 @@ def validateEmail(email):
 def main(email, password, confirm, to, subject):
     try:
 
-        gmail_user = click.prompt("FROM")                       # email of sender account
+        gmail_user = click.prompt("FROM")  # email of sender account
 
         # check the format of email
-        valid = validateEmail(gmail_user)
+        valid = validate_email(gmail_user)
         if not valid:
             print("\ninvalid email format")
-            press = int(input("\nIf you want to try to send email again with vaild email"
+            press = int(input("If you want to try to send email again with vaild email"
                               " press 1, if you want to exist press 0: "))
             for i in range(2):
                 if press == 1:
                     main()
                 if press == 0:
                     exit()
+        else:
 
-        gmail_pwd =click.prompt("Please Enter your password")    # password of sender account
-        gmail_conf = click.prompt("Confirm Your Password")        # confirm password of sender account
+            gmail_pwd = click.prompt("Please Enter your password")  # password of sender account
 
-        # if two passwords isn`t equal ask user again
-        if gmail_pwd != gmail_conf:
-            press = int(input("\nIf you want to try to send email again press 1, if you want to exist press 0: "))
-            for i in range(2):
-                if press == 1:
-                    main()
-                if press == 0:
-                    exit()
-
-        # if all data is right send email
-        if gmail_pwd == gmail_conf and valid:
             smtpserver = smtplib.SMTP("smtp.gmail.com", 587)  # port number for gmail
 
             smtpserver.ehlo()
@@ -54,10 +37,11 @@ def main(email, password, confirm, to, subject):
 
             smtpserver.login(gmail_user, gmail_pwd)  # connect to the sender email
 
+
             to = click.prompt("To")
 
             # check the format of receiver email
-            valid = validateEmail(to)
+            valid = validate_email(to)
             if not valid:
                 print("\ninvalid email format")
                 press = int(input("\nIf you want to try to send email again with vaild email"
@@ -68,22 +52,28 @@ def main(email, password, confirm, to, subject):
                     if press == 0:
                         exit()
             else:
-                subject = click.prompt("Subject")             # take the body of email
+
+                subject = click.prompt("Subject")  # take the body of email
+                body = click.prompt("Body")
+
                 smtpserver.sendmail(gmail_user, to, subject)  # send email
 
-                print("\nSend successfully\n")
+                print("\nSend successfully")
 
                 press = int(input("\nIf you want to send another email press 1, if you want to exist press 0: "))
-                print("\n")
 
                 for i in range(2):
                     if press == 1:
                         main()
                     if press == 0:
                         exit()
-    except:
-        print("\nEmail failed to send")
+    except Exception as err1:
+        print("Email failed to send")
+        print(err1)
 
 
-if __name__ == '__main__':
-    main()
+try:
+    if __name__ == '__main__':
+        main()
+except ValueError as err:
+    print(err)
